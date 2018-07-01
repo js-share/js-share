7719229c7dafe7cc4ab3cfdd252583ee1f2e92ce
Sam Goldberg <sgoldber61@gmail.com>
2018-07-01 02:30:47 -0700

    basic react app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Hello from './Hello.js';
+
+class App extends Component {
+  render() {
+    return (
+      <Hello />
+    );
+  }
+}
+
+export default App;
