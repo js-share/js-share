1eabcdf4295fc845b8db9b4ce10db7f0ab524924
Sam Goldberg <sgoldber61@gmail.com>
2018-07-01 15:50:02 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react_app_tutorial
+
+This is a boilerplate for Codesmith designed to combine a front end create-react-app with a back end express server.
+
+Run with ```npm run dev``` on the root folder
--- router.js
+++ router.js
@@ -3,4 +3,5 @@ module.exports = function(app) {
   app.get('/api/hello', (req, res) => {
     res.send({message: 'Hi! Here is some content from the server.'});
   });
+  // routes for creating, writing ,updating, and deleting
 }
